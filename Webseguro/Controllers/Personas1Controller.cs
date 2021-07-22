f330e38fcb160ec10c2d0851f58dbe807bb2197c
Dylan Mejia <86011694+Extremite787@users.noreply.github.com>
2021-07-22 18:22:01 -0500

    oj

---
+++ Webseguro/Controllers/Personas1Controller.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Webseguro.Data;
+using Webseguro.Models;
+
+namespace Webseguro.Controllers
+{
+    public class Personas1Controller : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public Personas1Controller(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            List<Persona> personas = new List<Persona>();
+            personas = _applicationDbContext.Persona.ToList();
+            return View(personas);
+        }
+        public IActionResult Details(int Codigo)
+        {
+            if (Codigo == 0)
+
+                return RedirectToAction("Index");
+            Persona persona = _applicationDbContext.Persona.Where(x => x.Codigo == Codigo).FirstOrDefault();
+            if (persona == null)
+                return RedirectToAction("Index");
+            return View(persona);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Persona persona)
+        {
+            try
+            {
+                _applicationDbContext.Add(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                return View(persona);
+            }
+
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            Persona persona = _applicationDbContext.Persona.Where(x => x.Codigo == id).FirstOrDefault();
+            if (persona == null)
+
+                return RedirectToAction("Index");
+
+            return View(persona);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Persona persona)
+        {
+            if (id != persona.Codigo)
+                return RedirectToAction("Index");
+            try
+            {
+                _applicationDbContext.Update(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                return View(persona);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == 0)
+                return RedirectToAction("Index");
+            Persona persona = _applicationDbContext.Persona.Where(x => x.Codigo == id).FirstOrDefault();
+            try
+            {
+                _applicationDbContext.Remove(persona);
+                _applicationDbContext.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Webseguro/Data/ApplicationDbContext.cs
+++ Webseguro/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Webseguro.Models;
 
 namespace Webseguro.Data
 {
@@ -12,5 +13,36 @@ namespace Webseguro.Data
             : base(options)
         {
         }
+        public virtual DbSet<Persona> Persona { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Persona>(en =>
+            {
+                en.HasKey(e => e.Codigo);
+
+                en.Property(e => e.Nombre)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                en.Property(e => e.Apellido)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+                en.Property(e => e.Direccion)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+                en.Property(e => e.Estado)
+                //.IsRequired()
+                //.HasMaxLength(250)
+                .IsUnicode(false);
+
+            });
+        }        
     }
 }
